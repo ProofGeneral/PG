613797636bc4cae2c732b2b5952eca21370fcb7b
Paul Steckler <steck@stecksoft.com>
2016-08-31 11:52:52 -0400

    new file

---
+++ github-issues/open/8.v
@@ -0,0 +1,9 @@
+(* https://github.com/psteckler/ProofGeneral/issues/8 *)
+
+(* Process first line, hit C-c C-c, try to process second line, 
+   you get an Elisp error
+*)
+
+Goal nat.
+Check nat.
+
