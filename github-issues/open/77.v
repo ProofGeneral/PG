18b803ba03a1cfee6d526ac097f83e3530a6248c
Paul Steckler <steck@stecksoft.com>
2017-02-03 11:46:43 -0500

    add comment

--- github-issues/open/77.v
+++ github-issues/open/77.v
@@ -1,3 +1,11 @@
+(* test case for issue #77 *)
+
+(* Jump to the final "." after the intros. Now hit
+ * "backsapce". I would expect PG to undo the intros tactic and move the
+ * proof state to before that tactic. Instead, the proof state doesn't
+ * change, and "intros" (without the dot) is still marked as processed. 
+ *)
+
 Lemma foo : True -> True.
 Proof.
   intros.
