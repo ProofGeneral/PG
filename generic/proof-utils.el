a2c371f69e29c68f15f8769713d852475e87f592
Paul Steckler <steck@stecksoft.com>
2017-02-27 17:17:03 -0500

    fix parens

--- generic/proof-utils.el
+++ generic/proof-utils.el
@@ -280,32 +280,32 @@ Ensure that point is visible in window."
 		(min-pos (point-min))
 		(ws-chars '(32 9 10))) ; space, tab, newline
 	    (if pos
-	      ;; find beginning of line
-	      (progn 
-		(unless (= vis-pos min-pos)
-		  (cl-decf vis-pos)
+		;; find beginning of line
+		(progn 
+		  (unless (= vis-pos min-pos)
+		    (cl-decf vis-pos)
+		    (while (and (> vis-pos min-pos)
+				(not (= (char-after vis-pos) 10)))
+		      (cl-decf vis-pos))
+		    (cl-incf vis-pos))
+		  ;; skip backwards over whitespace
 		  (while (and (> vis-pos min-pos)
-			      (not (= (char-after vis-pos) 10)))
-		    (cl-decf vis-pos))
-		  (cl-incf vis-pos))
-	      ;; skip backwards over whitespace
-	      (while (and (> vis-pos min-pos)
-			  (memq (char-after vis-pos) ws-chars))
-		(cl-decf vis-pos)))
-	    ;; Ensure vis-pos is visible.  Again, window may have died
-	    ;; inside shrink to fit, for some reason
-	    (when (window-live-p window)
-	      (unless (pos-visible-in-window-p vis-pos window)
-		(recenter -1))
-	      (with-current-buffer buffer
-		(if (window-bottom-p window)
-		    (unless (local-variable-p 'mode-line-format)
-		      ;; Don't show any mode line.
-		      (set (make-local-variable 'mode-line-format) nil))
-		  (unless mode-line-format
-		    ;; If the buffer gets displayed elsewhere, re-add
-		    ;; the modeline.
-		    (kill-local-variable 'mode-line-format)))))))))))
+			      (memq (char-after vis-pos) ws-chars))
+		    (cl-decf vis-pos)))
+	      ;; Ensure vis-pos is visible.  Again, window may have died
+	      ;; inside shrink to fit, for some reason
+	      (when (window-live-p window)
+		(unless (pos-visible-in-window-p vis-pos window)
+		  (recenter -1))
+		(with-current-buffer buffer
+		  (if (window-bottom-p window)
+		      (unless (local-variable-p 'mode-line-format)
+			;; Don't show any mode line.
+			(set (make-local-variable 'mode-line-format) nil))
+		    (unless mode-line-format
+		      ;; If the buffer gets displayed elsewhere, re-add
+		      ;; the modeline.
+		      (kill-local-variable 'mode-line-format))))))))))))
 
 (defun proof-clean-buffer (buffer)
   "Erase buffer and hide from display if proof-delete-empty-windows set.
