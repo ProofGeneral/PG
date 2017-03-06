4871b0f843cf9d9c30a4627cc522b86385535691
Paul Steckler <steck@stecksoft.com>
2017-03-06 13:23:33 -0500

    remove backtrace erroneously added

--- generic/pg-response.el
+++ generic/pg-response.el
@@ -253,7 +253,6 @@ dragging the separating bars.
   'hybrid. The default value is 'smart which sets the automatic
   behaviour described above."
   (interactive)
-  (backtrace)
   (cond
    (proof-multiple-frames-enable
     ;; If we are coming from single frame mode, delete associated
