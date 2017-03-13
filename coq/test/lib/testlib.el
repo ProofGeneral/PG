b1a6edd613845dfdfa37f6f81f6ce487debf527c
Paul Steckler <steck@stecksoft.com>
2017-03-13 16:08:07 -0400

    expected vs. got printing

--- coq/test/lib/testlib.el
+++ coq/test/lib/testlib.el
@@ -4,13 +4,17 @@
   (switch-to-buffer (find-file file))
   (proof-process-buffer))
 
-(defun debug-msg (msg)
-  (print msg 'external-debugging-output))
+(defun debug-msg (msg &rest args)
+  (let ((str (format msg args)))
+    (princ str 'external-debugging-output)
+    (princ "\n" 'external-debugging-output)))
 
 (defun verify-response (expected)
   (with-current-buffer proof-response-buffer
     (let ((got (buffer-substring-no-properties (point-min) (point-max))))
       (unless (equal expected got)
-	(debug-msg "*** Contents of response buffer are NOT correct ***")))))
+	(debug-msg "*** Contents of response buffer are NOT correct ***")
+	(debug-msg "EXPECTED: %s" expected)
+	(debug-msg "GOT:      %s" got)))))
 
 
