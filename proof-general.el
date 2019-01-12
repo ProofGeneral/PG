beebf0f144a04190c0f0910860f603a14f937b6b
Erik Martin-Dorel <erik@martin-dorel.org>
2019-01-12 21:35:30 +0100

    Fix the license notice in proof-general.el
    as PG master is still under GPLv2 (while PG async already switched to GPLv3+)
    
    href: https://github.com/ProofGeneral/PG/tree/fb3b75dab55b6e6befffc53e136422558be5faa0#contributing

--- proof-general.el
+++ proof-general.el
@@ -14,10 +14,9 @@
 ;; Package-Requires: ((emacs "24.3"))
 ;; Version: 4.5-git
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This software is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; version 2, as published by the Free Software Foundation.
 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
