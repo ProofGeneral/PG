a26b8e99a7876fa1761fd3ac55b9a2d781237ee9
Hendrik Tews <hendrik@askra.de>
2020-12-05 18:05:39 +0100

    add tests for parallel background compilation

---
+++ ci/compile-tests/001-mini-project/a.v
@@ -0,0 +1,24 @@
+(* This file is part of Proof General.
+ *
+ * © Copyright 2020  Hendrik Tews
+ *
+ * Authors: Hendrik Tews
+ * Maintainer: Hendrik Tews <hendrik@askra.de>
+ *
+ * License:     GPL (GNU GENERAL PUBLIC LICENSE)
+ *
+ *
+ * This file is part of an automatic test case for parallel background
+ * compilation in coq-par-compile.el. See test.el in this directory.
+ *)
+
+(* The test script relies on absolute line numbers. 
+ * DO NOT INSERT ANY LINE UNLESS YOU KNOW WHAT YOU ARE DOING.
+ *)
+
+
+
+(* This is line 21 *)
+Require b.
+Require c.
+(* This is line 24 *)
