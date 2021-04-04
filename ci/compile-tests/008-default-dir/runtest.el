f3b20a24671d5d39f9e0e02b8a25bd6da42fc48e
Hendrik Tews <hendrik@askra.de>
2021-04-04 11:39:18 +0200

    extend default-directory test to second stage

--- ci/compile-tests/008-default-dir/runtest.el
+++ ci/compile-tests/008-default-dir/runtest.el
@@ -63,18 +63,21 @@ See `cct-generic-check-main-buffer'."
   "`default-directory' test.
 Test that `default-directory' is correctly set independently of the
 current buffer in the foreground."
-  (let (vo-times av-buffer ci-buffer other-locked-files)
+  (let (vo-times av-buffer ci-buffer other-locked-files
+        vok-times vos-vio-files)
 
     ;; (setq cct--debug-tests t)
     ;; (setq coq--debug-auto-compilation t)
     (find-file "a.v")
     (setq av-buffer (current-buffer))
 
-    (message "coqdep: %s\ncoqc: %s\nPATH %s\nexec-path: %s"
-             coq-dependency-analyzer
-             coq-compiler
-             (getenv "PATH")
-             exec-path)
+    (message
+     "coqdep: %s\ncoqc: %s\nPATH %s\nexec-path: %s\ndetected coq version: %s"
+     coq-dependency-analyzer
+     coq-compiler
+     (getenv "PATH")
+     exec-path
+     coq-autodetected-version)
 
     (find-file "../..")
     (setq ci-buffer (current-buffer))
@@ -99,6 +102,7 @@ current buffer in the foreground."
     (cct-check-locked 23 'locked)
     (cct-replace-last-word 23 "3")
     (check-main-buffer vo-times "6" '("./b.vo") other-locked-files)
+    (setq vo-times (cct-record-change-times all-compiled-ancestors))
 
     ;; 4. change c and process again - b and c should get compiled
     (message "\n4. change c and process again - b and c should get compiled\n")
@@ -107,5 +111,33 @@ current buffer in the foreground."
     (cct-check-locked 23 'locked)
     (cct-replace-last-word 23 "5")
     (check-main-buffer vo-times "8" '("./b.vo" "./c.vo") other-locked-files)
+    (setq vo-times (cct-record-change-times all-compiled-ancestors))
 
+    ;; 5. change b and c and reprocess with vos-and-vok/quick-and-vio2vo
+    (message
+     "\n5. change b and c and reprocess with vos-and-vok/quick-and-vio2vo\n")
+    (if (coq--post-v811)
+        (setq coq-compile-vos 'vos-and-vok
+              vos-vio-files '("./b.vos" "./c.vos")
+              vok-times (cct-record-change-times '("./b.vok" "./c.vok")))
+      (setq coq-compile-quick 'quick-and-vio2vo
+            vos-vio-files '("./b.vio" "./c.vio")))
+
+    (set-buffer "b.v")
+    (cct-check-locked 23 'locked)
+    (cct-replace-last-word 23 "7")
+    (set-buffer "c.v")
+    (cct-check-locked 23 'unlocked)
+    (cct-replace-last-word 23 "10")
+    (check-main-buffer nil "17" nil other-locked-files)
+
+    (cct-files-are-readable vos-vio-files)
+    ;; this will switch to a different default-directory, see the
+    ;; hooks above
+    (cct-wait-for-second-stage)
+    (if (coq--post-v811)
+        (progn
+          (cct-older-change-times vok-times)
+          (cct-unmodified-change-times vo-times))
+      (cct-older-change-times vo-times))
     ))
